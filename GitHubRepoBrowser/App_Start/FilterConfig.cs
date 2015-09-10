8e484947a531af67bf618ec9633e8415e25ce96c
Eric Siebeneich <eric.siebeneich@gmail.com>
2015-09-10 17:29:58 -0500

    GitHub Repo Browser

---
+++ GitHubRepoBrowser/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GitHubRepoBrowser {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
